4548b958b8dee4a0e46d9a348a295fb19a56e82a
aceLeVillegas <sarahvillegas95@gmail.com>
2018-03-18 22:27:35 -0700

    Add feature to pan tray for faces

---
+++ Canvas/Canvas/View Controllers/CanvasViewController.swift
@@ -0,0 +1,70 @@
+//
+//  CanvasViewController.swift
+//  Canvas
+//
+//  Created by Sarah Villegas  on 3/18/18.
+//  Copyright © 2018 Sarah Villegas . All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Here we use the method didPan(sender:), which we defined in the previous step, as the action.
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPanTray(_:)))
+        
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        trayView.isUserInteractionEnabled = true
+        trayView.addGestureRecognizer(panGestureRecognizer)
+        
+        trayDownOffset = 212
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+        } else if sender.state == .ended {
+            
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayDown
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayUp
+                }
+            }
+            print("Gesture ended")
+        }
+    }
+    
+
+}
